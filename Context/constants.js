21488abe9a206c2ce8cccffbdef138af45e72653
TanmayMachkar <tanmaymacho@gmail.com>
2024-04-06 09:06:35 +0530

    fix 4

--- Context/constants.js
+++ Context/constants.js
@@ -3,5 +3,5 @@
 //0x5D2B378AB8fC721968EC8c32DeE579cb1fA6509F
 import crowdFunding from './CrowdFunding.json';
 
-export const CrowdFundingAddress = '0x5D2B378AB8fC721968EC8c32DeE579cb1fA6509F';
+export const CrowdFundingAddress = '0x3019BcADF97d6D5Fb9384b28eeb8D9E1b6d8246D';
 export const CrowdFundingABI = crowdFunding.abi;
